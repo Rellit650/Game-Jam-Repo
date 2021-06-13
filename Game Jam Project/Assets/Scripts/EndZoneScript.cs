5a4c9c27fe68f0774268c93553dcb3da3be8b1e6
Andrew Tiller <andrew.tiller@mymail.champlain.edu>
2021-06-13 00:41:59 -0400

    Small Tweaks

--- Game Jam Project/Assets/Scripts/EndZoneScript.cs
+++ Game Jam Project/Assets/Scripts/EndZoneScript.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class EndZoneScript : MonoBehaviour
 {
+    [SerializeField]
+    private int splitsRequired;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) 
         {
-            Debug.Log("You did! Wow!");
+            if (other.transform.parent.GetComponent<PlayerSplitController>().splitsLeft >= splitsRequired) 
+            {
+                Debug.Log("You did It! Wow!");
+            }           
         }
     }
 }
